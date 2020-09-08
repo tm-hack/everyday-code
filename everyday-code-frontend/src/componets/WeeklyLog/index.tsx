f385a7730da2fdd92f6347bccf3a69f1292536af
takumimi1124 <takufrom1124@gmail.com>
2020-09-08 23:09:27 +0900

    weeklyの実績画面を作成する

--- everyday-code-frontend/src/componets/WeeklyLog/index.tsx
+++ everyday-code-frontend/src/componets/WeeklyLog/index.tsx
@@ -10,12 +10,34 @@ import {
   Menu,
   Segment,
   Icon,
+  Tab,
 } from 'semantic-ui-react'
 
 import { Link } from 'react-router-dom';
-import { userData } from '../../data/user_account'
 
-const git_account = userData.account_name;
+const commit_message = 'コミットメッセージを表示する';
+const lang_num = 'Typescript xxxxx Line';
+
+const message = (
+  <List>
+    <List.Item>
+      {commit_message}
+    </List.Item>
+    <List.Item>
+      {lang_num}
+    </List.Item>
+  </List>
+)
+
+const panes = [
+  { menuItem: '月', render: () => <Tab.Pane>{message}</Tab.Pane> },
+  { menuItem: '火', render: () => <Tab.Pane>{message}</Tab.Pane> },
+  { menuItem: '水', render: () => <Tab.Pane>{message}</Tab.Pane> },
+  { menuItem: '木', render: () => <Tab.Pane>{message}</Tab.Pane> },
+  { menuItem: '金', render: () => <Tab.Pane>{message}</Tab.Pane> },
+  { menuItem: '土', render: () => <Tab.Pane>{message}</Tab.Pane> },
+  { menuItem: '日', render: () => <Tab.Pane>{message}</Tab.Pane> },
+]
 
 const WeeklyLog: React.FC = () => {
   console.log('WeeklyLogを通った')
@@ -29,14 +51,15 @@ const WeeklyLog: React.FC = () => {
         </Container>
       </Menu>
 
-      <Container text style={{ marginTop: '5em' }}>
-        <Header as='h1'>
-          <Icon name='user circle' size='huge' />{git_account}
-        </Header>
-      </Container>
-
-      <Container>
-        <Header>WeeklyLogを表示する</Header>
+      <Container className="weeklyLog" text style={{ marginTop: '5em' }}>
+        <Header as='h1' dividing>Weekly Log</Header>
+        <Grid container columns={1} doubling>
+          <Grid.Column>
+            <Segment>
+              <Tab panes={panes} />
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </Container>
     </div>)
 }
